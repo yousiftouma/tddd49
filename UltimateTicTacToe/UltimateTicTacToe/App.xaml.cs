5198e4ea668d1fce4f6f0734707b4df5e1cfe9ac
yousiftouma <yousif_touma@hotmail.com>
2017-10-27 00:06:05 +0200

    Add project skeleton

---
+++ UltimateTicTacToe/UltimateTicTacToe/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UltimateTicTacToe.View;
+using Unity;
+
+namespace UltimateTicTacToe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IUnityContainer container = new UnityContainer();
+            // Register services in IoC container (Dependency Injection)
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
