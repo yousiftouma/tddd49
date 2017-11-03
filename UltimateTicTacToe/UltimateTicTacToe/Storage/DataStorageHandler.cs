4338417a2c05b3b86faa7e934c0b2185b86f8cb3
Yousif Touma <youto814@student.liu.se>
2017-11-03 16:39:24 +0100

    Begin linq serialization

--- UltimateTicTacToe/UltimateTicTacToe/Storage/DataStorageHandler.cs
+++ UltimateTicTacToe/UltimateTicTacToe/Storage/DataStorageHandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UltimateTicTacToe.Model;
 
 namespace UltimateTicTacToe.Storage
 {
@@ -13,6 +16,23 @@ namespace UltimateTicTacToe.Storage
         public DataStorageHandler(IFileHandler fileHandler)
         {
             _fileHandler = fileHandler;
+        }
+
+        public void StoreBoard(bool isPlayerOneTurn, MarkerType winner, MarkerType playerOne, 
+            MarkerType playerTwo, SubBoard[,] subboards)
+        {
+            // TODO
+            // SubBoard[3,3] subboards (inkl winner och active i separata)
+            JObject game = 
+                new JObject(
+                    new JProperty("game",
+                        new JObject(
+                            new JProperty("isPlayerOneTurn", isPlayerOneTurn.ToString()),
+                            new JProperty("winner", (int)winner),
+                            new JProperty("playerOne", (int)playerOne),
+                            new JProperty("playerTwo", (int)playerTwo),
+                            new JProperty("subboards", 
+                            new))));
         }
 
     }
