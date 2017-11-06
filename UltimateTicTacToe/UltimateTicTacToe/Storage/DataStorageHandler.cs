62529447faeaa330579d23612b18e31c820a3405
Andreas Järvelä <andja235@student.liu.se>
2017-11-06 11:56:05 +0100

    Finish up json creation

--- UltimateTicTacToe/UltimateTicTacToe/Storage/DataStorageHandler.cs
+++ UltimateTicTacToe/UltimateTicTacToe/Storage/DataStorageHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -18,7 +19,7 @@ namespace UltimateTicTacToe.Storage
             _fileHandler = fileHandler;
         }
 
-        public void StoreBoard(bool isPlayerOneTurn, MarkerType winner, MarkerType playerOne, 
+        public void StoreBoard(bool isPlayerOneTurn, MarkerType winner, MarkerType playerOne,
             MarkerType playerTwo, SubBoard[,] subboards)
         {
             var boards = new List<List<SubBoard>> {new List<SubBoard>(), new List<SubBoard>(), new List<SubBoard>()};
@@ -26,29 +27,53 @@ namespace UltimateTicTacToe.Storage
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    boards[i].Add(subboards[i,j]);
+                    boards[i].Add(subboards[i, j]);
                 }
             }
 
+            JArray one = new JArray();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    JArray aSubboard = new JArray();
+                    SubBoard lol = subboards[i, j];
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 3; l++)
+                        {
 
+                            aSubboard.Add(lol.GetMarker(new Position(k, l)));
+                        }
+                    }
+                    one.Add(aSubboard);
+                }
+            }
 
             //TODO
-            //SubBoard[3, 3] subboards(inkl winner och active i separata)
-            JObject game =
-                new JObject(
-                    new JProperty("game",
-                        new JObject(
-                            new JProperty("isPlayerOneTurn", isPlayerOneTurn.ToString()),
-                            new JProperty("winner", (int)winner),
-                            new JProperty("playerOne", (int)playerOne),
-                            new JProperty("playerTwo", (int)playerTwo),
-                            new JProperty("activeSubboards",
-                                new JArray(
-                                    from sbs in boards
-                                    from sb in sbs
-                                    select sb.IsActive)))));
+                //SubBoard[3, 3] subboards(inkl winner och active i separata)
+                JObject game =
+                    new JObject(
+                        new JProperty("game",
+                            new JObject(
+                                new JProperty("isPlayerOneTurn", isPlayerOneTurn.ToString()),
+                                new JProperty("winner", (int) winner),
+                                new JProperty("playerOne", (int) playerOne),
+                                new JProperty("playerTwo", (int) playerTwo),
+                                new JProperty("activeSubboards",
+                                    new JArray(
+                                        from sbs in boards
+                                        from sb in sbs
+                                        select sb.IsActive)),
+                                new JProperty("winnerSubboards",
+                                    new JArray(
+                                        from sbs in boards
+                                        from sb in sbs
+                                        select sb.Winner)),
+                                new JProperty("board", one))));
+
+            }
 
         }
-
     }
-}
+
