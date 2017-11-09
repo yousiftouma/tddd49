661b99d786e6f6a15d5888e2603f1d3137cb49a6
Yousif Touma <youto814@student.liu.se>
2017-11-09 13:45:01 +0100

    Add exception handling in data layer

---
+++ UltimateTicTacToe/UltimateTicTacToe/Model/CustomExceptions/LoadStateFailException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UltimateTicTacToe.Model.CustomExceptions
+{
+    public class LoadStateFailException : Exception
+    {
+        public LoadStateFailException()
+        {
+        }
+
+        public LoadStateFailException(string message) : base(message)
+        {
+        }
+
+        public LoadStateFailException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- UltimateTicTacToe/UltimateTicTacToe/Storage/DataStorageHandler.cs
+++ UltimateTicTacToe/UltimateTicTacToe/Storage/DataStorageHandler.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using UltimateTicTacToe.Model;
+using UltimateTicTacToe.Model.CustomExceptions;
 
 namespace UltimateTicTacToe.Storage
 {
@@ -17,89 +20,121 @@ namespace UltimateTicTacToe.Storage
 
         public void StoreBoard(BoardDto boardDto)
         {
-            // TODO handle possible exceptions
-            var subboards = boardDto.Subboards;
-            var boards = new List<List<SubBoard>> { new List<SubBoard>(), new List<SubBoard>(), new List<SubBoard>() };
-            var subboardsJArray = new JArray();
-            for (int i = 0; i < 3; i++)
+            try
             {
-                for (int j = 0; j < 3; j++)
+                SubBoard[,] subboards;
+                try
                 {
-                    boards[i].Add(subboards[i, j]);
-                    var aSubboardJArray = new JArray();
-                    var aSubboard = subboards[i, j];
-                    for (int k = 0; k < 3; k++)
+                    subboards = boardDto.Subboards;
+                }
+                catch (Exception e)
+                {
+                    throw new ArgumentException("Could not access SubBoard from boardDto.", e);
+                }
+                var boards =
+                    new List<List<SubBoard>> { new List<SubBoard>(), new List<SubBoard>(), new List<SubBoard>() };
+                var subboardsJArray = new JArray();
+                for (var i = 0; i < 3; i++)
+                {
+                    for (var j = 0; j < 3; j++)
                     {
-                        for (int l = 0; l < 3; l++)
+                        boards[i].Add(subboards[i, j]);
+                        var aSubboardJArray = new JArray();
+                        var aSubboard = subboards[i, j];
+                        for (var k = 0; k < 3; k++)
                         {
-                            aSubboardJArray.Add(aSubboard.GetMarker(new Position(k, l)));
+                            for (var l = 0; l < 3; l++)
+                            {
+                                aSubboardJArray.Add(aSubboard.GetMarker(new Position(k, l)));
+                            }
                         }
+                        subboardsJArray.Add(aSubboardJArray);
                     }
-                    subboardsJArray.Add(aSubboardJArray);
+                }
+                try
+                {
+                    var game =
+                        new JObject(
+                            new JProperty("game",
+                                new JObject(
+                                    new JProperty("isPlayerOneTurn", boardDto.IsPlayerOneTurn.ToString()),
+                                    new JProperty("winner", (int)boardDto.Winner),
+                                    new JProperty("playerOne", (int)boardDto.PlayerOne),
+                                    new JProperty("playerTwo", (int)boardDto.PlayerTwo),
+                                    new JProperty("activeSubboards",
+                                        new JArray(
+                                            from sbs in boards
+                                            from sb in sbs
+                                            select sb.IsActive)),
+                                    new JProperty("subboardWinners",
+                                        new JArray(
+                                            from sbs in boards
+                                            from sb in sbs
+                                            select sb.Winner)),
+                                    new JProperty("board", subboardsJArray))));
+                    _fileHandler.Write(game.ToString());
+                }
+                catch (Exception e)
+                {
+                    throw new SerializationException(
+                        "Failed to serialize and write to file, see inner exception for details.", e);
                 }
             }
-
-            var game =
-                new JObject(
-                    new JProperty("game",
-                        new JObject(
-                            new JProperty("isPlayerOneTurn", boardDto.IsPlayerOneTurn.ToString()),
-                            new JProperty("winner", (int) boardDto.Winner),
-                            new JProperty("playerOne", (int) boardDto.PlayerOne),
-                            new JProperty("playerTwo", (int) boardDto.PlayerTwo),
-                            new JProperty("activeSubboards",
-                                new JArray(
-                                    from sbs in boards
-                                    from sb in sbs
-                                    select sb.IsActive)),
-                            new JProperty("subboardWinners",
-                                new JArray(
-                                    from sbs in boards
-                                    from sb in sbs
-                                    select sb.Winner)),
-                            new JProperty("board", subboardsJArray))));
-            _fileHandler.Write(game.ToString());
+            catch (Exception e)
+            {
+                throw new SaveStateFailException("Failed to StoreBoard, see inner exception for details.", e);
+            }
         }
 
         public bool LoadBoard(IRules rules, out BoardDto boardDto)
         {
-            // TODO handle possible exceptions
-
-            boardDto = new BoardDto();
-            if (!_fileHandler.FileExists())
-            {
-                return false;
-            }
             try
             {
-                var jsonDataString = _fileHandler.Read();
-                var jsonObject = JObject.Parse(jsonDataString);
-                boardDto.IsPlayerOneTurn = (bool) jsonObject["game"]["isPlayerOneTurn"];
-                boardDto.Winner = (MarkerType) (int) jsonObject["game"]["winner"];
-                boardDto.PlayerOne = (MarkerType) (int) jsonObject["game"]["playerOne"];
-                boardDto.PlayerTwo = (MarkerType) (int) jsonObject["game"]["playerTwo"];
+                boardDto = new BoardDto();
+                if (!_fileHandler.FileExists())
+                {
+                    return false;
+                }
+                List<bool> activeSubboards;
+                List<MarkerType> subboardWinners;
+                List<List<MarkerType>> subboardsAsMarkerTypeArrays;
+                try
+                {
+                    var jsonDataString = _fileHandler.Read();
+                    var jsonObject = JObject.Parse(jsonDataString);
 
-                var activeSubboards =
-                    jsonObject["game"]["activeSubboards"].Select(isActive => (bool) isActive).ToList();
-                var subboardWinners = jsonObject["game"]["subboardWinners"].Select(winner => (MarkerType) (int) winner)
-                    .ToList();
-                var subboards = from sb in jsonObject["game"]["board"] select (JArray) sb;
-                var subboardsAsMarkerTypeArrays = subboards.Select(b => b.ToObject<List<MarkerType>>()).ToList();
+                    boardDto.IsPlayerOneTurn = (bool) jsonObject["game"]["isPlayerOneTurn"];
+                    boardDto.Winner = (MarkerType) (int) jsonObject["game"]["winner"];
+                    boardDto.PlayerOne = (MarkerType) (int) jsonObject["game"]["playerOne"];
+                    boardDto.PlayerTwo = (MarkerType) (int) jsonObject["game"]["playerTwo"];
+
+                    activeSubboards = jsonObject["game"]["activeSubboards"].Select(isActive => (bool) isActive)
+                        .ToList();
+                    subboardWinners = jsonObject["game"]["subboardWinners"].Select(winner => (MarkerType) (int) winner)
+                        .ToList();
+                    var subboards = from sb in jsonObject["game"]["board"] select (JArray) sb;
+                    subboardsAsMarkerTypeArrays = subboards.Select(b => b.ToObject<List<MarkerType>>()).ToList();
+                }
+                catch (Exception e)
+                {
+                    throw new SerializationException("Failed to deserialize from file, see inner exception for details",
+                        e);
+                }
 
                 var subboardArray = new SubBoard[3, 3];
                 var tempSubboardList = new List<SubBoard>();
                 int counter;
-                for (int i = 0; i < 9; i++)
+                for (var i = 0; i < 9; i++)
                 {
                     counter = 0;
                     var subboardAsArray = subboardsAsMarkerTypeArrays[i];
                     var markerTypeArray = new MarkerType[3, 3];
-                    
-                    for (int j = 0; j < 3; j++)
+
+                    for (var j = 0; j < 3; j++)
                     {
-                        for (int k = 0; k < 3; k++)
+                        for (var k = 0; k < 3; k++)
                         {
-                            markerTypeArray[j,k] = subboardAsArray[counter];
+                            markerTypeArray[j, k] = subboardAsArray[counter];
                             counter++;
                         }
                     }
@@ -107,22 +142,21 @@ namespace UltimateTicTacToe.Storage
                 }
 
                 counter = 0;
-                for (int i = 0; i < 3; i++)
+                for (var i = 0; i < 3; i++)
                 {
-                    for (int j = 0; j < 3; j++)
+                    for (var j = 0; j < 3; j++)
                     {
                         subboardArray[i, j] = tempSubboardList[counter];
                         counter++;
                     }
                 }
                 boardDto.Subboards = subboardArray;
+                return true;
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Failed parsing json, got exception {e}");
-                return false;
+                throw new LoadStateFailException("Failed to LoadBoard, see inner exception for details", e);
             }
-            return true;
         }
     }
 }
